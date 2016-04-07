851dd4a71caa9cf027d2c8dd459fc4cf5a5f893a
Julian Skinner <ju.skinner@gmail.com>
2016-04-06 22:25:03 -0400

    React Routes
    This added a few new things...
    
    npm install --save react-router which adds the component to the
    package.json
    
    Added two new routes 'root' and 'home'.

--- app/index.js
+++ app/index.js
@@ -1,64 +1,7 @@
-var USER_DATA = {
-  name: 'Julian Skinner',
-  username: 'ju-Skinner',
-  image: 'https://avatars2.githubusercontent.com/u/1633290?v=3&s=460'
-}
-
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var ProfilePic = React.createClass({
-  render: function() {
-    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
-  }
-})
-
-var Link = React.createClass({
-  changeURL: function() {
-    window.location.replace(this.props.href)
-  },
-  render: function() {
-    return (
-      <span style={{color: 'blue', cursor: 'pointer'}}
-        onClick={this.changeURL}>
-        {this.props.children}
-      </span>
-    )
-  }
-})
-var ProfileLink = React.createClass({
-  render: function() {
-    return (
-        <div>
-          <Link href={'https://www.github.com/' + this.props.username}>
-            {this.props.username}
-          </Link>
-      </div>
-    )
-  }
-})
-
-var ProfileName = React.createClass({
-  render: function() {
-    return (
-        <div>{this.props.name}</div>
-    )
-  }
-})
-
-var Avatar = React.createClass({
-  render: function() {
-    return (
-        <div>
-          <ProfilePic imageUrl={this.props.user.image} />
-          <ProfileName name={this.props.user.name} />
-          <ProfileLink username={this.props.user.username} />
-        </div>
-    )
-  }
-})
-
+var routes = require('./config/routes')
 ReactDOM.render(
-    <Avatar  user={USER_DATA} />,
+    routes,
     document.getElementById('app')
 );
