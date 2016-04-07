79133d11bc6bbaa9da0a1adde8bc6d9ac5baf722
Julian Skinner <ju.skinner@gmail.com>
2016-04-06 23:32:40 -0400

    Form Submission
    This is where wired up the submit button, but first leveraged the
    ContextTypes. This assist with reducing the number of things that would
    need to be passed around. In this case it would of been 'Router'.

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -2,6 +2,9 @@ var React = require('react');
 var transparentBg = require('../styles').transparentBg
 
 var PromptContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
   getInitialState: function() {
     return {
       username: ''
@@ -12,12 +15,33 @@ var PromptContainer = React.createClass({
       username: e.target.value
     })
   },
+  onSubmitUser: function(e) {
+    e.preventDefault();
+    var username = this.state.username;
+    this.setState({
+      username: ''
+    });
+
+    if (this.props.routeParams.playerOne) {
+      // go to /battle
+      this.context.router.push({
+        pathname: '/battle',
+        query: {
+          playerOne: this.props.routeParams.playerOne,
+          playerTwo: this.state.username
+        }
+      })
+    } else {
+      // go to player Two
+      this.context.router.push('/playerTwo/' + this.state.username)
+    }
+  },
   render: function() {
     return (
       <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
         <h1>{this.props.route.header}</h1>
         <div className="col-sm-12">
-          <form>
+          <form onSubmit={this.onSubmitUser}>
             <div className="form-group">
               <input className="form-control"
                       placeholder="Github Username"
