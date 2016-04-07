7298cf036968119ae7a75ebc550f26d03c8f235f
Julian Skinner <ju.skinner@gmail.com>
2016-04-06 22:32:55 -0400

    Hash History
    Updated home route to be the IndexRoute which means that it will serve
    as the default component.
    
    Added hashHistory as the browsers state, meaning the hash tag will help
    identify when the state has changed.

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,13 +3,14 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory; // Default history storage
 var Main = require('../components/Main');
 var Home = require('../components/Home');
 
 var routes = (
-  <Router>
+  <Router hashHistory={hashHistory}>
     <Route path="/" component={Main}>
-      <Route path="/home" component={Home} />
+      <IndexRoute component={Home} />
     </Route>
   </Router>
 );
