2fb84ffddd78031f10cc5eb2417a568a63ec8ae6
Julian Skinner <ju.skinner@gmail.com>
2016-04-11 22:34:16 -0400

    Helper functions to get additional data
    These functions are used to calculate arbituary scores for the battle
    between two users.

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -8,6 +8,42 @@ function getUserInfo (username) {
   return axios.get('https://api.github.com/users/' + username + param)
 }
 
+// fetch users repositories
+function getRepos (username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100')
+}
+
+// calculates all the stars that
+// the user has.
+function getTotalStars(repos) {
+  return repos.data.reduce(function (prev, current) {
+    return prev + current.stargazers_count
+  }, 0)
+}
+
+// accumulates getRepos and getTotalStars
+// and returns a single object with the data
+function getPlayersData(player) {
+  return getRepos(player.login)
+    .then(getTotalStars)
+    .then(function(totalStars) {
+      return {
+        followers: player.followers,
+        totalStars: totalStars
+      }
+    })
+}
+
+// returns: array
+// calculates values based on simple
+// algorithm
+function calculateScores(players) {
+  return [
+    players[0].followers * 3 + players[0].totalStars,
+    players[1].followers * 3 + players[1].totalStars
+  ]
+}
+
 var helpers = {
   getPlayersInfo: function(players) {
     return axios.all(players.map(function (username) {
@@ -19,6 +55,14 @@ var helpers = {
     }).catch(function (err) {
       console.warn('Error in getPlayersInfo', err)
     })
+  },
+  battle: function (players) {
+    var playerOneData = getPlayersData(players[0]);
+    var playerTwoData = getPlayersData(players[1]);
+
+    return axios.all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch(function (err) { console.warn('Error in getPlayersInfo: ', err)})
   }
 };
 
