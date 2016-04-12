1f41aa0f8726ae80a8734eddc20ca60db7c03375
Julian Skinner <ju.skinner@gmail.com>
2016-04-11 22:53:00 -0400

    Some nice UI enhancements
    Used a private component Results.js:StartOver
    User a reuasable Container (MainContainer.js)

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,13 +1,49 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var Link = require('react-router').Link;
+var styles = require('../styles');
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
+var MainContainer = require('./MainContainer');
 
-function puke(obj) {
-  return <pre>{JSON.stringify(obj, 2, ' ')}</pre>
-}
-
-function Results (props) {
+function StartOver () {
   return (
-    <div> RESULTS: {puke(props)} </div>
+    <div className="col-sm-12" style={styles.space}>
+      <Link to="/playerOne">
+        <button type="button" className="btn btn-lg btn-danger">Start Over</button>
+      </Link>
+    </div>
+  )
+}
+function Results (props) {
+  if ( props.isLoading === true ) {
+    return (
+      <p> LOADING </p>
+    )
+  }
+  if ( props.scores[0] === props.scores[1]) {
+    return (
+      <MainContainer>
+        <h1> It's a tie! </h1>
+        <StartOver />
+      </MainContainer>
+    )
+  }
+  var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
+  var losingIndex = winningIndex === 0 ? 1 : 0;
+  return (
+    <MainContainer>
+      <h1>Results</h1>
+      <div className="col-sm-8 col-sm-offset-2">
+        <UserDetailsWrapper header="Winner">
+          <UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]}></UserDetails>
+        </UserDetailsWrapper>
+        <UserDetailsWrapper header="Loser">
+          <UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]}></UserDetails>
+        </UserDetailsWrapper>
+      </div>
+      <StartOver />
+    </MainContainer>
   )
 }
 
