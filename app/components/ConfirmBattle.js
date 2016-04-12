c38c6283d0a7b6542f8413f457cfc9a2b17d075c
Julian Skinner <ju.skinner@gmail.com>
2016-04-11 21:44:46 -0400

    User Details view
    Custom view for the User Details.

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -2,11 +2,13 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 var styles = require('../styles');
 var Link = require('react-router').Link;
+var UserDetails = require('./UserDetails');
 
 function puke(object) {
   return <pre>{JSON.stringify(object, null, ' ')}</pre>
 }
 function ConfirmBattle(props) {
+
   return props.isLoading === true
     ? <p> LOADING! </p>
     : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
@@ -14,11 +16,11 @@ function ConfirmBattle(props) {
         <div className="col-sm-8 col-sm-offset-2">
           <div className="col-sm-6">
             <p className="lead">Player 1</p>
-            {puke(props.playersInfo[0])}
+            <UserDetails info={props.playersInfo[0]} />
           </div>
           <div className="col-sm-6">
             <p className="lead">Player 2</p>
-            {puke(props.playersInfo[1])}
+            <UserDetails info={props.playersInfo[1]} />
           </div>
         </div>
         <div className="col-sm-8 col-sm-offset-2">
